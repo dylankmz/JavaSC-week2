17c4f5bef5a0c8689761ccd21e4a4a50c7d4d266
Dylan Karapekmez <55973031+dylankmz@users.noreply.github.com>
2020-02-17 12:56:17 +0100

    colors

--- _startcode/oefening2/colors.js
+++ _startcode/oefening2/colors.js
@@ -1,1 +1,11 @@
 const colors = ['red', 'blue', 'yellow', 'green', 'magenta', 'cyan'];
+let block = document.querySelectorAll('.js-block');
+
+document.body.addEventListener("click", function (event) {
+    console.log(event.target.classList);
+
+    if (event.target.classList.contains("js-block")) {
+        event.target.style.backgroundColor = "red";
+    }
+
+});
