ff9d574dd4fa0880b612fba1f5384654326a7988
Dylan Karapekmez <55973031+dylankmz@users.noreply.github.com>
2020-02-18 13:58:07 +0100

    test

--- _startcode/oefening2/colors.js
+++ _startcode/oefening2/colors.js
@@ -1,11 +1,14 @@
 const colors = ['red', 'blue', 'yellow', 'green', 'magenta', 'cyan'];
 let block = document.querySelectorAll('.js-block');
 
-document.body.addEventListener("click", function (event) {
+document.body.addEventListener("mouseover", function (event) {
     console.log(event.target.classList);
 
     if (event.target.classList.contains("js-block")) {
-        event.target.style.backgroundColor = "red";
+        let random = Math.floor(Math.random() * colors.length);
+        event.target.style.backgroundColor = colors[random];
+        console.log(random);
+
     }
 
 });
